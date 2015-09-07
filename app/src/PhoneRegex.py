bd28bc67dc07b46bcb1255cec4795f83cf382888
Cole Murray <colemurray.cs@gmail.com>
2015-09-06 21:25:54 -0700

    Add format phone number method and test cases

--- app/src/PhoneRegex.py
+++ app/src/PhoneRegex.py
@@ -7,11 +7,26 @@ Parse a phone number from a given line
 '''
 
 
-def parse_phone_number(phone_num):
-    phone_regex_pattern = r'([\(]?[0-9]{3,3}[\)]?)?[0-9]{3,3}[-]*[0-9]{4,4}'
-    match = re.search(phone_regex_pattern, phone_num)
+def parse_phone_number(string_to_parse):
+    phone_regex_pattern = r'([\(]?[0-9]{3,3}[\)]?)?[-]?[0-9]{3,3}[-]?[0-9]{4,4}'
+    match = re.search(phone_regex_pattern, string_to_parse)
+
     if match is None:
         return ''
     return match.group()
 
 
+'''
+Returns a phone number formatted
+with only numbers and hyphens
+'''
+
+
+def format_phone_number(phone_number):
+    phone_regex_pattern = r'[0-9]+'
+    match = re.findall(phone_regex_pattern, phone_number)
+    formatted = ''
+    for c in match:
+        formatted += str(c)
+
+    return formatted
--- app/src/test/TestPhoneRegex.py
+++ app/src/test/TestPhoneRegex.py
@@ -1,4 +1,4 @@
-from PhoneRegex.app.src.PhoneRegex import parse_phone_number
+from PhoneRegex.app.src.PhoneRegex import parse_phone_number, format_phone_number
 
 __author__ = 'User'
 
@@ -18,5 +18,12 @@ class TestPhoneRegex(unittest.TestCase):
         expected = '(555)555-5555'
         result = parse_phone_number(fake_string)
         print result
+        assert result == expected
+
+
+    def test_format_phone_should_return_generic_fake_number(self):
+        fake_number = '(555)-555-5555'
+        expected = '5555555555'
+        result = format_phone_number(fake_number)
         assert result == expected
 
